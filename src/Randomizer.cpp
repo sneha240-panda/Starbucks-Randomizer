9b572deac3521414aa4f5968867f2cc9f401142b
doubleeepie <117495394+doubleeepie@users.noreply.github.com>
2023-03-09 10:27:37 -0800

    Update Randomizer.cpp

--- src/Randomizer.cpp
+++ src/Randomizer.cpp
@@ -1,10 +1,14 @@
 #include "../lib/Randomizer.h"
 #include "../lib/Weather.h"
 #include "../lib/CaffGenerator.h"
+#include "../lib/NonCaffGenerator.h"
 #include <string>
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+
+//need to create a way to differentiate NonCaff from Caff. We can do this by creating an if statement that takes in the user's age as an object 
+//and if it is under say like the age of 10, the program would use the Non-Caffeinated generator, and if over that age, it uses the CaffGenerator.
 
 using namespace std;
 
@@ -35,4 +39,25 @@ string Randomizer::getRandomDrink()
 
     delete caffGenerator;
     return drinkList.at(randIndex);
+    
+    /* vector<string> drinkList = {};
+    NonCaffGenerator *noncaffGenerator = new NonCaffGenerator();
+    bool isHot = false;
+    switch (weather->getCurrentPos())
+    {
+    case 0:
+        isHot = false;
+        break;
+    case 1:
+        isHot = true;
+        break;
+    default:
+        isHot = false;
+    }
+    noncaffGenerator->generateList(&drinkList, &isHot);
+    srand(time(0));
+    int randIndex = rand() % drinkList.size();
+
+    delete noncaffGenerator;
+    return drinkList.at(randIndex); */
 }
