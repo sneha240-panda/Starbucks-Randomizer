09d56a64abac4ba68f205c9c7ed755a7eef9c3c1
doubleeepie <117495394+doubleeepie@users.noreply.github.com>
2023-03-09 09:33:44 -0800

    Update NonCaffGenerator.cpp

--- src/NonCaffGenerator.cpp
+++ src/NonCaffGenerator.cpp
@@ -1,11 +1,9 @@
-#include "../lib/CaffGenerator.h"
-#include "../lib/Americano.h"
-#include "../lib/ChaiTea.h"
-#include "../lib/CoffeeSyrup.h"
+#include "../lib/NonCaffGenerator.h"
 #include "../lib/CremeFrappuccino.h"
 #include "../lib/CremeFrappuccinoFlavor.h"
-#include "../lib/Espresso.h"
-#include "../lib/Latte.h"
+#include "../lib/Refresher.h"
+#include "../lib/RefresherFlavor.h"
+#include "../lib/HotChocolate.h"
 
 #include <vector>
 #include <string>
@@ -20,5 +18,15 @@ void NonCaffGenerator::generateList(vector<string> *drinkList, bool *isHot)
     drinkList->push_back(frapFlavor->getName());
     delete cremeFrap;
     delete frapFlavor;
+    
+    Refresher *refresh = new Refresher();
+    RefresherFlavor *refresherFlavor = new RefresherFlavor(refresh);
+    drinkList->push_back(refresherFlavor->getName());
+    delete refresh;
+    delete refresherFlavor;
+    
+    HotChocolate *hotChocolate = new HotChocolate(*isHot);
+    drinkList->push_back(hotChocolate->getName());
+    delete hotChocolate;
 
 }
