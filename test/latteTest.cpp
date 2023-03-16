6757fdc5e9fe5ee173b9d451bab7657e681f4895
Zaniah Lewis <zlewi004@ucr.edu>
2023-03-15 21:01:56 -0700

    Added latte test

---
+++ test/latteTest.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+#include "../lib/Espresso.h"
+#include "../lib/Coffee.h"
+#include "../lib/Drink.h"
+#include <string>
+
+
+TEST(LatteTest, ConstructorTest1) { 
+        Latte latte1（true, 2)
+        EXPECT_EQ（latte.getNumEspresso（）, "double"）
+}
