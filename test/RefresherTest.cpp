4c0034a074a5763d4bf07f2db7d09729fc5559b4
deet5 <idenismiller@gmail.com>
2023-03-15 22:24:13 -0700

    Fixed Refresher tests

--- test/RefresherTest.cpp
+++ test/RefresherTest.cpp
@@ -5,21 +5,26 @@
 
 TEST(RefresherGetNameTest, Rtest1)
 {
-    Refresher refresher1();
+    Refresher refresher1;
     EXPECT_EQ(refresher1.getName(), "Refresher");
 }
 
 TEST(RefresherGetNameTest, Rtest2)
 {
-    Refresher refresher2();
-    EXPECT_STRNE(refresher2.getName(), "Refreshers");
+    Refresher refresher2;
+    std::string getName = refresher2.getName();
+    char *name = new char[getName.length() + 1];
+    strcpy(name, getName.c_str());
+    char ref[] = "Refreshers";
+    EXPECT_STRNE(ref, name);
 }
 
 TEST(RefresherGetNameTest, Rtest3)
 {
-    Refresher refresher3();
-    EXPECT_STRCASEEQ(frefresher3.getName(), "refresher");
+    Refresher refresher3;
+    std::string getName = refresher3.getName();
+    char *name = new char[getName.length() + 1];
+    strcpy(name, getName.c_str());
+    char ref[] = "refresher";
+    EXPECT_STRCASEEQ(ref, name);
 }
-
-
-
