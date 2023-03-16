3bcfa8f354ad2d7d5ecf75eda1259435ac5c26fe
deet5 <idenismiller@gmail.com>
2023-03-15 22:36:00 -0700

    Fixed ChaiTea tests

--- test/chaiTest.cpp
+++ test/chaiTest.cpp
@@ -5,26 +5,24 @@
 
 TEST(ChaiConstructorTest, CTtest2)
 {
-    Chai chai2(true);
+    ChaiTea chai2(true);
     EXPECT_EQ(chai2.getTemp(), "Hot");
 }
 
 TEST(ChaiConstructorTest, CTtest3)
 {
-    Chai chai3(false);
+    ChaiTea chai3(false);
     EXPECT_EQ(chai3.getTemp(), "Iced");
 }
 
 TEST(ChaiGetNameTest, CTtest4)
 {
-    Chai chai4(true);
+    ChaiTea chai4(true);
     EXPECT_EQ(chai4.getName(), "Hot Chai Tea");
 }
 
 TEST(ChaiGetNameTest, CTtest5)
 {
-    Chai chai5(false);
+    ChaiTea chai5(false);
     EXPECT_EQ(chai5.getName(), "Iced Chai Tea");
 }
-
-
