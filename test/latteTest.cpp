8bd427637178d4b95aa4fb630df2555c08107e68
Zaniah Lewis <zlewi004@ucr.edu>
2023-03-15 21:25:02 -0700

    Added more latte test

--- test/latteTest.cpp
+++ test/latteTest.cpp
@@ -5,7 +5,19 @@
 #include <string>
 
 
-TEST(LatteTest, ConstructorTest1) { 
-        Latte latte1（true, 2)
-        EXPECT_EQ（latte.getNumEspresso（）, "double"）
+TEST(LatteTest, ConstructorTest1){ 
+        Latte latte(true,2);
+        EXPECT_EQ(latte.getNumEspresso(), "double");
 }
+
+TEST(LatteTest, ConstructorTest2){
+       	Latte latte2(false, 1);
+	EXPECT_EQ(latte2.getNumEspresso（）, "single");
+}
+
+TEST(LatteTest, ConstructorTest3){
+        Latte latte3(true, 3);
+        EXPECT_EQ(latte3.getNumEspresso（）, "double");
+}
+
+
