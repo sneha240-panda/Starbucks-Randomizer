1862e921695a9f5a07ddcd80c61a2b40ff18ae27
sneha240-panda <snehaapanda@gmail.com>
2023-03-16 10:15:36 -0700

    added more tests

--- test/randomizerTest.cpp
+++ test/randomizerTest.cpp
@@ -1,19 +1,22 @@
 #include "gtest/gtest.h"
 #include "../lib/Randomizer.h"
+#include "../lib/Weather.h"
+#include <string>
 
 TEST(RandomTest, constructorWorks)
 {
-    Randomizer *random1 = new Randomizer("1", 10, 2);
+    Weather *weather1 = new Weather();
+    Randomizer *random1 = new Randomizer(weather1, 10, 2);
     EXPECT_EQ(random1->getAge(), 10);
     EXPECT_EQ(random1->getuserChoiceDrink(), 2);
-    EXPECT_EQ(random1->getCurrWeather, 1);
+    EXPECT_EQ(random1->getCurrWeather(), 2);
 }
 TEST(RandomTest, getDrink)
 {
-    vector <string> empty = {};
-    Randomizer *generator1 = new Randomizer();
-    bool listAdd = true;
-    generator1->generateList(&empty, &listAdd);
-    EXPECT_GT(empty.size(), 0);
+    Weather *weather2 = new Weather();
+    Randomizer *generator1 = new Randomizer(weather2,10,2);
+    std::string one = generator1->getRandomDrink();
+    EXPECT_GT(one.size(), 0);
+
 }
 
