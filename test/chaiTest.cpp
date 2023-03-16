5624bf0e20eaa169d0bb7ab20d58afba3698f53d
doubleeepie <117495394+doubleeepie@users.noreply.github.com>
2023-03-15 19:25:47 -0700

    Update chaiTest.cpp

--- test/chaiTest.cpp
+++ test/chaiTest.cpp
@@ -5,24 +5,37 @@
 
 TEST(ChaiConstructorTest, CTtest1)
 {
-    Chai chai1(true);
+    Chai chai1();
     EXPECT_EQ(chai1.getTemp(), "Hot");
-}
+
 
 TEST(ChaiConstructorTest, CTtest2)
 {
-    Chai chai2(false);
+    Chai chai2(true);
     EXPECT_EQ(chai2.getTemp(), "Hot");
+}
+
+TEST(ChaiConstructorTest, CTtest3)
+{
+    Chai chai3(false);
+    EXPECT_EQ(chai3.getTemp(), "Hot");
 }
 
 TEST(ChaiGetNameTest, CTtest4)
 {
     Chai chai4(true);
-    EXPECT_EQ(americano4.getName(), "Hot Chai Tea");
+    EXPECT_EQ(chai4.getName(), "Hot Chai Tea");
 }
 
 TEST(ChaiGetNameTest, CTtest5)
 {
     Chai chai5(false);
-    EXPECT_EQ(americano4.getName(), "Hot Chai Tea");
+    EXPECT_EQ(chai5.getName(), "Hot Chai Tea");
 }
+
+TEST(ChaiGetNameTest, CTtest6)
+{
+    Chai chai6();
+    EXPECT_EQ(chai6.getName(), "Hot Chai Tea");
+}
+
